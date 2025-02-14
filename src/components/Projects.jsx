a8c20c3ba42616420cd3b893c5a866ad3a7ae610
rushikeshtreb <rushikeshvadnal7@gmail.com>
2025-02-14 14:53:35 +0530

    changes

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,10 +7,7 @@ import fatigued_models from "../assets/fatigued-project-models.png";
 import atman_model from "../assets/atman-project-model.png";
 import { SiGit, SiNetlify, SiVercel } from "react-icons/si";
 
-
 gsap.registerPlugin(ScrollTrigger);
-
-
 
 const Section = styled.section`
   position: relative;
@@ -20,24 +17,21 @@ const Section = styled.section`
   background: linear-gradient(to bottom, black, purple, black);
 `;
 
-
 const Container = styled.div`
   display: flex;
   height: 100%;
   width: 100%;
-  -ms-overflow-style: none;  /* IE & Edge */
+  -ms-overflow-style: none; /* IE & Edge */
   ::-webkit-scrollbar {
-  display: none;
-}
+    display: none;
+  }
 
-
-    
   @media (max-width: 950px) {
     -ms-overflow-style: auto;
     ::-webkit-scrollbar {
-    display: block;
+      display: block;
     }
-}   
+  }
 `;
 
 const ProjectWrapper = styled.div`
@@ -49,11 +43,9 @@ const ProjectWrapper = styled.div`
   padding: 0 2.5rem;
   flex-shrink: 0;
 
-    
   @media (max-width: 950px) {
     padding: 0;
-}
-
+  }
 `;
 
 const ProjectCard = styled.div`
@@ -69,9 +61,9 @@ const ProjectCard = styled.div`
   width: 60%;
   text-align: center;
   transition: transform 0.3s ease-in-out;
-  
 
-  @media (max-width: 1000px ) {  /* Mobile */
+  @media (max-width: 1000px) {
+    /* Mobile */
     width: 100%;
   }
 
@@ -81,6 +73,7 @@ const ProjectCard = styled.div`
     font-size: 14px;
     width: 100%;
     height: 100%;
+    gap: 10px;
   }
 `;
 
@@ -100,26 +93,35 @@ const ProjectInfo = styled.div`
   h2 {
     font-size: 1.875rem;
     font-weight: bold;
-    margin:10px;
+    margin: 10px;
   }
   p {
     margin-top: 0.5rem;
     font-size: 0.875rem;
     color: gray;
-    margin:10px;
+    margin: 10px;
   }
-  .icon-links{
+  .icon-links {
     display: flex;
     gap: 1rem;
-    margin:10px;
+    margin: 10px;
 
-    .git-icon , .deployment-icon{
+    .git-icon,
+    .deployment-icon {
       &:hover {
-    transform: scale(1.05);
-  }
+        transform: scale(1.05);
+      }
     }
-  }
 
+    
+  }
+  @media (max-width: 950px) {
+    height: 50%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: end;
+  }
 `;
 
 const ProjectImage = styled.img`
@@ -130,6 +132,11 @@ const ProjectImage = styled.img`
   transition: transform 0.5s ease-in-out;
   &:hover {
     transform: rotate(270deg) scale(1);
+  }
+
+  @media (max-width: 950px) {
+    margin-top: 0;
+    height: 50%;
   }
 `;
 
@@ -192,9 +199,24 @@ const Projects = () => {
         "Developed a responsive and user-friendly e-commerce website for a clothing brand as a freelance project. Features include a drag-and-drop t-shirt customization tool, an interactive 3D logo, secure authentication with bcrypt and JWT, and seamless Razorpay integration for payments.",
       image: resolute_rowe_models,
       link: "https://resolute-and-rowe-frontend.vercel.app/",
-      deployment: <SiVercel onClick={() => redirectToProject('https://resolute-and-rowe-frontend.vercel.app/')} className="deployment-icon" size={30} />,
-      github: <SiGit onClick={() => redirectToProject('https://github.com/rushivadnal7/RESOLUTE-ROWE')} className="git-icon" size={30} />
-
+      deployment: (
+        <SiVercel
+          onClick={() =>
+            redirectToProject("https://resolute-and-rowe-frontend.vercel.app/")
+          }
+          className="deployment-icon"
+          size={30}
+        />
+      ),
+      github: (
+        <SiGit
+          onClick={() =>
+            redirectToProject("https://github.com/rushivadnal7/RESOLUTE-ROWE")
+          }
+          className="git-icon"
+          size={30}
+        />
+      ),
     },
     {
       id: 2,
@@ -203,10 +225,26 @@ const Projects = () => {
         "Building a fully responsive MERN stack workout tracker to enhance fitness journeys with fatigue tracking, progress history, and interactive charts. Features include exercise-based fatigue analysis, visual progress graphs, and scalable API structuring.",
       image: fatigued_models,
       link: "https://musclance-frontend-development.netlify.app/",
-      deployment: <SiNetlify onClick={() => redirectToProject("https://musclance-frontend-development.netlify.app/")} className="deployment-icon" size={30} />,
-      github: <SiGit onClick={() => redirectToProject('https://github.com/rushivadnal7/musclance')} className="git-icon" size={30} />
-
-
+      deployment: (
+        <SiNetlify
+          onClick={() =>
+            redirectToProject(
+              "https://musclance-frontend-development.netlify.app/"
+            )
+          }
+          className="deployment-icon"
+          size={30}
+        />
+      ),
+      github: (
+        <SiGit
+          onClick={() =>
+            redirectToProject("https://github.com/rushivadnal7/musclance")
+          }
+          className="git-icon"
+          size={30}
+        />
+      ),
     },
     {
       id: 3,
@@ -215,9 +253,26 @@ const Projects = () => {
         "Developed the landing page for The Ātman Foundation, showcasing its mission to empower underprivileged children through education and mentorship, ensuring a seamless and engaging user experience.",
       image: atman_model,
       link: "https://atman-foundation.netlify.app/",
-      deployment: <SiNetlify onClick={() => redirectToProject("https://atman-foundation.netlify.app/")} className="deployment-icon" size={30} />,
-      github: <SiGit onClick={() => redirectToProject('https://github.com/rushivadnal7/atman-foundation')} className="git-icon" size={30} />
-
+      deployment: (
+        <SiNetlify
+          onClick={() =>
+            redirectToProject("https://atman-foundation.netlify.app/")
+          }
+          className="deployment-icon"
+          size={30}
+        />
+      ),
+      github: (
+        <SiGit
+          onClick={() =>
+            redirectToProject(
+              "https://github.com/rushivadnal7/atman-foundation"
+            )
+          }
+          className="git-icon"
+          size={30}
+        />
+      ),
     },
   ];
 
@@ -235,12 +290,8 @@ const Projects = () => {
                 <h2>{project.title}</h2>
                 <p>{project.description}</p>
                 <div className="icon-links">
-                  <div className="github-link">
-                    {project.github}
-                  </div>
-                  <div className="deployment-link">
-                    {project.deployment}
-                  </div>
+                  <div className="github-link">{project.github}</div>
+                  <div className="deployment-link">{project.deployment}</div>
                 </div>
               </ProjectInfo>
               <ProjectImage
